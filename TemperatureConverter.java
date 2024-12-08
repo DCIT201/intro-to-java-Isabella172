a327c17af6099f82d10e7ee987dfc9c9b98c29a9
Isabella172 <berlah87009@gmail.com>
2024-12-08 09:44:34 +0000

    final

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,7 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class TemperatureConverter {
+ public class TemperatureConverter {
     public static void main (String [] args) {
         Scanner input = new Scanner(System.in);
 
